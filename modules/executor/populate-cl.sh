2c0baf0d0a3b37d985b02e52d120ca3fad390228
Joseph C Wang <joequant@gmail.com>
2024-11-01 18:02:40 +0800

    add bridge

--- modules/executor/populate-cl.sh
+++ modules/executor/populate-cl.sh
@@ -2,6 +2,12 @@
 
 docker compose exec link-main-node chainlink admin login -f /chainlink/.api
 docker compose exec link-main-node chainlink admin status
+docker compose exec link-main-node chainlink bridges create '{
+  "name": "test1",
+  "url": "http://test1/",
+  "confirmations": 1,
+  "minimumContractPayment": "0"
+}'
 
 
 
