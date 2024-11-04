d51d697220f1ae06332075bd4afa156ea801a906
Joseph C Wang <joequant@gmail.com>
2024-11-04 21:55:16 +0800

    add jobs and run via cli

--- chainnode/populate-cl.sh
+++ chainnode/populate-cl.sh
@@ -3,11 +3,10 @@
 docker compose exec link-main-node chainlink admin login -f /chainlink/.api
 docker compose exec link-main-node chainlink admin status
 docker compose exec link-main-node chainlink bridges create '{
-  "name": "test1",
-  "url": "http://test1/",
+  "name": "executor",
+  "url": "http://executor:8000/",
   "confirmations": 1,
   "minimumContractPayment": "0"
 }'
-
-
+docker compose exec link-main-node chainlink jobs create /chainlink/chainnode/node-api1.toml
 
