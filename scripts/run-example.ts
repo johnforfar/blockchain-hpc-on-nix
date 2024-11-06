5f79f868d3fbc3977dbf01e51a20de3d701ed2e3
Joseph C Wang <joequant@gmail.com>
2024-11-06 18:15:35 +0800

    add example approve

--- scripts/run-example.ts
+++ scripts/run-example.ts
@@ -3,19 +3,27 @@
 import { ethers, upgrades } from 'hardhat'
 
 async function main (): void {
+    const [owner] = await ethers.getSigners()
     const contractAddress = process.env.EXAMPLE_CONTRACT
     const name = 'HpcExample'
     const HpcExample = await ethers.getContractFactory(name)
     const hpcExample = HpcExample.attach(contractAddress)
-    const address = await hpcExample.getToken()
-    console.log(address)
+    const tokenAddress = await hpcExample.getToken()
+    console.log(tokenAddress)
+    const HpcToken = await ethers.getContractFactory('HpcToken')
+    const hpcToken = HpcToken.attach(tokenAddress)
+    const tokens = console.log(
+        await hpcToken.balances(owner)
+    )
+    const fee = BigInt("10000000000000000")
+    const tx1 = await hpcToken.approve(contractAddress, fee)
     const tx = await hpcExample.doTransferAndRequest(
 	"test",
 	"test",
 	"test",
 	"cbor",
 	"10000000000000000000",
-	BigInt("10000000000000000")
+	fee
     )
     await tx.wait()
 }
