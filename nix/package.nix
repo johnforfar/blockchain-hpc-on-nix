b2a584fb065f073ef6dcc6fe1d01b1d7a395327f
johnforfar <46335740+johnforfar@users.noreply.github.com>
2024-11-15 18:11:51 +1100

    fixed chainlink & rpc-failover issues

--- nix/package.nix
+++ nix/package.nix
@@ -8,7 +8,6 @@ pkgs.buildNpmPackage {
   npmFlags = [ "--legacy-peer-deps" ];
   makeCacheWritable = true;
 
-  # Copy node_modules and build files, but don't run build during Nix build
   dontBuild = true;
 
   nativeBuildInputs = with pkgs; [
@@ -17,10 +16,9 @@ pkgs.buildNpmPackage {
     nodejs_20
     python3
     yarn
-    gnumake    # Added for make commands
+    gnumake
   ];
 
-  # Add pre-build step
   preBuild = ''
     make build || true
   '';
@@ -29,20 +27,31 @@ pkgs.buildNpmPackage {
     runHook preInstall
 
     mkdir -p $out/{share,bin}
+    mkdir -p $out/share/hardhat-app/runtime/chainlink
     
     # Copy entire app including node_modules
     cp -r . $out/share/hardhat-app/
     
+    # Create default chainlink config files if they don't exist
+    touch $out/share/hardhat-app/runtime/chainlink/password
+    touch $out/share/hardhat-app/runtime/chainlink/secrets.toml
+    touch $out/share/hardhat-app/runtime/chainlink/config.toml
+    touch $out/share/hardhat-app/runtime/chainlink/api
+    
+    # Make sure the chainlink directory and files are accessible
+    chmod -R 755 $out/share/hardhat-app/runtime
+
     # Make scripts executable
     chmod +x $out/share/hardhat-app/scripts/*.sh
     chmod +x $out/share/hardhat-app/modules/*/build.sh
 
-    # Create a wrapped docker-compose command that includes env vars
+    # Create a wrapped docker-compose command
     makeWrapper ${pkgs.docker-compose}/bin/docker-compose $out/bin/xnode-blockchain-hpc \
       --add-flags "--project-directory $out/share/hardhat-app up" \
-      --chdir "$out/share/hardhat-app"
+      --chdir "$out/share/hardhat-app" \
+      --set CHAINLINK_CONFIG_DIR "$out/share/hardhat-app/runtime/chainlink"
 
-    # Create wrapper for reset-testnet that preserves cwd
+    # Create wrapper for reset-testnet
     makeWrapper ${pkgs.bash}/bin/bash $out/bin/reset-testnet \
       --add-flags "$out/share/hardhat-app/scripts/reset-testnet.sh" \
       --prefix PATH : ${lib.makeBinPath [ 
